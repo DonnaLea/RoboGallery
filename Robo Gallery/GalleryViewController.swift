783d5e0f139ba5fb28483c7c8a0fb76fe936c026
Donna McCulloch <donnalea@mcchouse.com>
2018-07-18 23:14:43 -0700

    Simple UICollectionViewController setup

--- Robo Gallery/GalleryViewController.swift
+++ Robo Gallery/GalleryViewController.swift
@@ -8,18 +8,69 @@
 
 import UIKit
 
-class GalleryViewController: UIViewController {
+class GalleryViewController: UICollectionViewController {
 
+  // MARK: - Properties
+  fileprivate struct Constants {
+    static let cellSpacing = CGFloat(2.0)
+    static let itemsPerRow: CGFloat = 4
+  }
+
+
+  convenience init() {
+    let layout = UICollectionViewFlowLayout()
+    self.init(collectionViewLayout: layout)
+  }
+
+  // MARK: - View Lifecycle
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
-  }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
-  }
+    collectionView?.backgroundColor = .white
+    collectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: UICollectionViewCell.reuseIdentifier())
 
+  }
 
 }
+
+// MARK: - UICollectionViewDataSource
+extension GalleryViewController {
+
+  override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    return 50
+  }
+
+  override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    let cell = collectionView.dequeueReusableCell(withReuseIdentifier: UICollectionViewCell.reuseIdentifier(), for: indexPath)
+    cell.backgroundColor = .red
+
+    return cell
+  }
+
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension GalleryViewController : UICollectionViewDelegateFlowLayout {
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+
+    let paddingSpace = Constants.cellSpacing * (Constants.itemsPerRow - 1)
+    let availableWidth = view.frame.width - paddingSpace
+    let widthPerItem = availableWidth / Constants.itemsPerRow
+
+    return CGSize(width: widthPerItem, height: widthPerItem)
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+    return .zero
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+    return Constants.cellSpacing
+  }
+
+  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+    return Constants.cellSpacing
+  }
+}
+
 
