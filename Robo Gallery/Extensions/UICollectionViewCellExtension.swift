049ee50bf12553aa66f969cf6b15f8b52f4d7579
Donna McCulloch <donnalea@mcchouse.com>
2018-07-18 23:14:11 -0700

    UICollectionViewCell extension to get reuseIdentifier

---
+++ Robo Gallery/Extensions/UICollectionViewCellExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UICollectionViewCellExtension.swift
+//  Robo Gallery
+//
+//  Created by Donna McCulloch on 18/7/18.
+//  Copyright © 2018 Donna McCulloch. All rights reserved.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+
+  // Use the class as the reuseIdentifier String.
+  static func reuseIdentifier() -> String {
+    return "\(self)"
+  }
+
+}
